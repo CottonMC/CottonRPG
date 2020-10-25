01901dad866f4e26c83048331a775fc14681b306
Pyrofab <redstoneinfire@gmail.com>
2020-10-25 09:53:58 +0100

    Fix CharacterData ticking

--- src/main/java/io/github/cottonmc/cottonrpg/RpgComponents.java
+++ src/main/java/io/github/cottonmc/cottonrpg/RpgComponents.java
@@ -5,10 +5,14 @@ import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import io.github.cottonmc.cottonrpg.data.CharacterData;
 import io.github.cottonmc.cottonrpg.data.PlayerCharacterData;
 import nerdhub.cardinal.components.api.util.RespawnCopyStrategy;
+import net.minecraft.entity.player.PlayerEntity;
 
 public final class RpgComponents implements EntityComponentInitializer {
     @Override
     public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
-        registry.registerForPlayers(CharacterData.KEY, PlayerCharacterData::new, RespawnCopyStrategy.ALWAYS_COPY);
+        registry.beginRegistration(PlayerEntity.class, CharacterData.KEY)
+                .impl(PlayerCharacterData.class)
+                .respawnStrategy(RespawnCopyStrategy.ALWAYS_COPY)
+                .end(PlayerCharacterData::new);
     }
 }
